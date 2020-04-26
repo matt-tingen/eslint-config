7ff869d42fe76542da35696aa9ebee5b80af3b3f
Matt Tingen <matt.tingen@gmail.com>
2020-04-25 23:59:36 -0400

    Initial creation

---
+++ index.js
@@ -0,0 +1,87 @@
+const { rules: baseES6Rules } = require('eslint-config-airbnb-base/rules/es6');
+const {
+  rules: baseStyleRules,
+} = require('eslint-config-airbnb-base/rules/style');
+
+const modifyRule = (rule, modify) => modify(rule);
+
+const modifyRules = (ruleSet, modifiers) =>
+  Object.fromEntries(
+    Object.entries(modifiers).map(([name, modify]) => [
+      name,
+      modifyRule(ruleSet[name], modify),
+    ]),
+  );
+
+const airbnbOverrides = {
+  'import/order': [
+    'error',
+    {
+      'newlines-between': 'never',
+      alphabetize: { order: 'asc', caseInsensitive: true },
+      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+    },
+  ],
+  '@typescript-eslint/explicit-function-return-type': 'off',
+  '@typescript-eslint/no-non-null-assertion': 'off',
+  '@typescript-eslint/no-use-before-define': 'off',
+  'no-plusplus': 'off',
+  'no-shadow': 'off',
+  'no-void': 'off',
+  'consistent-return': 'off',
+  'no-restricted-globals': ['error', 'event', 'name', 'isFinite', 'isNaN'],
+  ...modifyRules(baseStyleRules, {
+    'lines-between-class-members'([level, when, config]) {
+      return [level, when, { ...config, exceptAfterSingleLine: true }];
+    },
+  }),
+  ...modifyRules(baseES6Rules, {
+    'object-shorthand'([level, when, config]) {
+      return [level, when, { ...config, avoidQuotes: false }];
+    },
+  }),
+};
+
+const additions = {
+  '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
+};
+
+const disableBuggedRules = {
+  // https://github.com/benmosher/eslint-plugin-import/issues/1453
+  'import/no-cycle': 'off',
+};
+
+// Enabling type-aware rules incurs a performance hit.
+// TODO: avoid disabling these is a separate config.
+const disableTypeAwareRules = {
+  '@typescript-eslint/no-implied-eval': 'off',
+  '@typescript-eslint/no-throw-literal': 'off',
+  '@typescript-eslint/no-floating-promises': 'off',
+};
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  ignorePatterns: ['node_modules/', 'dist/', '.*cache/'],
+  reportUnusedDisableDirectives: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  settings: {
+    'import/internal-regex': '^[~/]',
+  },
+  rules: {
+    ...airbnbOverrides,
+    ...additions,
+    ...disableBuggedRules,
+    ...disableTypeAwareRules,
+  },
+};
